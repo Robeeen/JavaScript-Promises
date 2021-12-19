62864c2666f0dd0e8b52f59d524fe819e343a2b9
Shams Arefin Khan <robeeen@gmail.com>
2021-12-18 23:33:48 -0800

    Use random

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
+//pending, rejected, fulfilled.
+
 const value = 2
 
+
 const promise = new Promise ((resolve, reject) => {
-    resolve('hello world')
-    //reject('Error')
+    const random = Math.floor(Math.random() * 3);
+    if (random === value){
+        resolve('hello world');
+    }else{
+        reject('Error');
+    }
+    
+    
 })
 
 console.log(promise);
