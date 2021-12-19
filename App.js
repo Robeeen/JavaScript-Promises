2b0fae915a9c896c424117859283d3254ed97c4a
Shams Arefin Khan <robeeen@gmail.com>
2021-12-19 00:03:50 -0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,9 @@
 const value = 2
 
 
+
 const promise = new Promise ((resolve, reject) => {
+    //Lets get the random number between 0 ~ 2.
     const random = Math.floor(Math.random() * 3);
     if (random === value){
         resolve('hello world');
@@ -16,4 +18,4 @@ const promise = new Promise ((resolve, reject) => {
 
 console.log(promise);
 
-promise.then((data) => {console.log(data)});
+promise.then((data) => console.log(data)).catch((err) => console.log(err));
