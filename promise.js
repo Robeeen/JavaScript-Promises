7269afaadde1a1d6690672186ab9e77cb75287c4
Shams Arefin Khan <robeeen@gmail.com>
2021-12-20 21:20:17 +0600

    Call all promise

--- promise.js
+++ promise.js
@@ -1,9 +1,24 @@
-const promise = new Promise((resolve, reject) =>{
+// const promise = new Promise((resolve, reject) =>{
+//     setTimeout(() => {
+//         resolve({user: "Fiaz"});
+//     }, 5000);
+// });
+
+// promise.then(result => {
+//     console.log(result);
+// })
+
+const youTube = new Promise(resolve => {
     setTimeout(() => {
-        resolve({user: "Fiaz"});
-    }, 5000);
+        console.log("Getting videos from YouTube");
+        resolve({videos : [1, 2, 3, 4, 5]});
+    }, 2000);
 });
 
-promise.then(result => {
-    console.log(result);
-})
+const faceBook = new Promise(resolve =>{
+    setTimeout(() => {
+        console.log("Users from Facebook");
+        resolve({user: 'Marks Zucks'});
+    }, 4000);
+});
+Promise.all([youTube, faceBook]).then(result => console.log(result));
