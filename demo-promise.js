148e86abcf918e1550a064d804435df3af1273ba
Shams Arefin Khan <robeeen@gmail.com>
2021-12-21 21:08:26 +0600

    Added Bold function

--- demo-promise.js
+++ demo-promise.js
@@ -3,19 +3,20 @@ const button = document.querySelector('.btn');
 //Create a function to Change Color, select the Div class and then add time effect
 
 button.addEventListener('click', () => {
-    addColor(1000, '.first', 'red')
-    .then(() => addColor(2000, '.second', 'green'))
-    .then(() => addColor(5000, '.third', 'blue'))
+    addColor(1000, '.first', 'red', 'bold')
+    .then(() => addColor(2000, '.second', 'green', 'bold'))
+    .then(() => addColor(5000, '.third', 'blue', 'bold'))
     .catch((err) => console.log(err))
 });
 
 
-function addColor(time, selector, color){
+function addColor(time, selector, color, bold){
     const items = document.querySelector(selector);
     return new Promise((resolve, reject) => {
         if (items){
             setTimeout(() => {
                 items.style.color = color;
+                items.style.fontWeight = bold;
             }, time)
             resolve()
         }else{
